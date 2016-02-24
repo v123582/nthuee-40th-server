1f2f05dbafb10af1887a32255507e3159b9d4c10
v123582 <v123582@gmail.com>
2016-02-25 03:43:11 +0800

    fix javascript loading error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,34 +89,8 @@
     <script src="bower_compoents/blueimp-file-upload/js/vendor/jquery.ui.widget.js"></script>
     <script src="bower_compoents/blueimp-file-upload/js/jquery.iframe-transport.js"></script>
     <script src="bower_compoents/blueimp-file-upload/js/jquery.fileupload.js"></script>
-    </script>
-    <script>
-    /*jslint unparam: true */
-    /*global window, $ */
-    $(function () {
-        'use strict';
-        alert( '{{ $api_token }}' );
-        // Change this to the location of your server-side upload handler:
-        var url = 'api/basic?api_token={{ $api_token }}';
-        $('#fileupload').fileupload({
-            url: url,
-            dataType: 'json',
-            done: function (e, data) {
-                $.each(data.result.files, function (index, file) {
-                    $('<p/>').text(file.name).appendTo('#files');
-                });
-            },
-            progressall: function (e, data) {
-                var progress = parseInt(data.loaded / data.total * 100, 10);
-                $('#progress .progress-bar').css(
-                    'width',
-                    progress + '%'
-                );
-            }
-        }).prop('disabled', !$.support.fileInput)
-            .parent().addClass($.support.fileInput ? undefined : 'disabled');
-    });
-    </script>                  
+
+    @yield('javascripts')
 
 </body>
 </html>
--- resources/views/uploads/basic.blade.php
+++ resources/views/uploads/basic.blade.php
@@ -52,5 +52,34 @@
             </div>
         </div>
     </div>
+@endsection
 
+@section('javascripts')
+    <script>
+    /*jslint unparam: true */
+    /*global window, $ */
+    $(function () {
+        'use strict';
+        alert( '{{ $api_token }}' );
+        // Change this to the location of your server-side upload handler:
+        var url = 'api/basic?api_token={{ $api_token }}';
+        $('#fileupload').fileupload({
+            url: url,
+            dataType: 'json',
+            done: function (e, data) {
+                $.each(data.result.files, function (index, file) {
+                    $('<p/>').text(file.name).appendTo('#files');
+                });
+            },
+            progressall: function (e, data) {
+                var progress = parseInt(data.loaded / data.total * 100, 10);
+                $('#progress .progress-bar').css(
+                    'width',
+                    progress + '%'
+                );
+            }
+        }).prop('disabled', !$.support.fileInput)
+            .parent().addClass($.support.fileInput ? undefined : 'disabled');
+    });
+    </script>  
 @endsection
