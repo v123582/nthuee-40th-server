796d0032871e70848862ec26e9cd933c2ebab1d7
v123582 <v123582@gmail.com>
2016-02-26 03:37:00 +0800

    update

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -28,7 +28,6 @@ return array(
     'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
     'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
     'League\\Flysystem\\' => array($vendorDir . '/league/flysystem/src'),
-    'Laralib\\L5scaffold\\' => array($vendorDir . '/laralib/l5scaffold/src'),
     'Illuminate\\' => array($vendorDir . '/laravel/framework/src/Illuminate'),
     'Faker\\' => array($vendorDir . '/fzaninotto/faker/src/Faker'),
     'Dotenv\\' => array($vendorDir . '/vlucas/phpdotenv/src'),
